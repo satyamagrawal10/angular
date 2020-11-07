a2ea8512c71398758487b986237f6f10350028ab
Satyam Agrawal <42074389+satyamagrawal10@users.noreply.github.com>
2020-11-07 18:14:30 +0530

    bday invite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  firstName: string='Deepak';
+  lastName: string='Agrawal';
+  name: string='';
+  show:Boolean=false;
+
+  switchVisibility(){
+    if(this.name !== ""){
+      this.show=!this.show;
+    }
+    else{
+      alert("We humbly request you to enter your Name");
+    }
+  }
 }
